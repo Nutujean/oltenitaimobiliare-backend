b10677a14bdb372e3fc2aab7444e46129ef878be
nutujean <j@brzfreight.com>
2025-10-10 08:43:37 -0500

    Fix: eliminat import invalid 'admin' din authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { protect, admin } from "../middleware/authMiddleware.js";
+import { protect, } from "../middleware/authMiddleware.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
