6ecc42fc212d10a6d973dbf67a6d7c7ce4f240ae
nutujean <j@brzfreight.com>
2025-10-23 04:47:00 -0500

    ♻️ Forțat redeploy pentru authRoutes (forgot-password)

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import nodemailer from "nodemailer"; // 🟢 adăugat pentru trimitere email
+console.log("✅ authRoutes încărcat corect pe server");
 
 const router = express.Router();
 
